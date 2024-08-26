c591ecaa97e735c8338e346d14525f51b9f18e37
LYXOfficial <lyx2010official@outlook.com>
2024-08-26 16:16:25 +0800

    gitignore

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import '../styles/global.css'
+export const metadata = {
+  title: 'Ariasakaの小窝',
+}
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="zh-cn">
+      <body>
+        <NavBar/>
+        <div id="main-container">{children}</div>
+        <Footer/>
+      </body>
+    </html>
+  )
+}
---
+++ src/components/asides/CardInfo.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import "../../styles/ASide/global.css"
+import "../../styles/ASide/Info.css"
+export default function CardInfo({author,avatar,postCount,categoryCount,tagCount,socialLinks}:any){
+    return (
+        <div className="card-widget card-aside card-info">
+            <div className="card-info-avatar">
+                <img alt="avatar" src={avatar} className="card-info-avatar-img"></img>
+            </div>
+            <span className="card-info-name">
+                {author}
+            </span>
+            <div className="card-info-datas">
+                <Link className="card-info-data" href="/archives">
+                    <span className="card-info-data-title">文章</span>
+                    <span className="card-info-data-count">{postCount}</span>
+                </Link>
+                <Link className="card-info-data" href="/tags">
+                    <span className="card-info-data-title">标签</span>
+                    <span className="card-info-data-count">{tagCount}</span>
+                </Link>
+                <Link className="card-info-data" href="/categories">
+                    <span className="card-info-data-title">分类</span>
+                    <span className="card-info-data-count">{categoryCount}</span>
+                </Link>
+            </div>
+            <div className="card-info-socials">
+                {
+                    socialLinks.map((link:any)=>{
+                        return (
+                            <a key={link.name} className="card-info-social" href={link.link}>
+                                {link.icon}
+                            </a>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+}
