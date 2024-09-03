8068615743c06d5400042a6c1ea75ad61b778e5d
LYXOfficial <lyx2010official@outlook.com>
2024-09-03 08:37:44 +0800

    add sitemap

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,19 @@
+export const revalidate=3600;
+
+import { siteConfigs } from '@/config'
+
+export async function GET() {
+    const res=await fetch(`${siteConfigs.backEndUrl}/get/sitemap/sitemapGen`,{next:{tags:["posts"]}});
+    if(res.ok){
+        return new Response(await res.text(), {
+            headers: {
+                'content-type': 'application/xml'
+            }
+        });
+    }
+    return new Response("", {
+        headers: {
+            'content-type': 'application/xml'
+        }
+    });
+}
