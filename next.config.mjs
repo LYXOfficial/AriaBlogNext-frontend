86f7881f2acb476103d9f186600dd398550a644c
LYXOfficial <lyx2010official@outlook.com>
2024-09-14 19:20:43 +0800

    cors fix

--- next.config.mjs
+++ next.config.mjs
@@ -59,7 +59,7 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: "/*",
+                source: "/refreshCache/*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
