072fa40ab1eb58d9d7cbf06b7d4ac44878015ec7
LYXOfficial <lyx2010official@outlook.com>
2024-09-19 21:43:09 +0800

    fix indents

--- src/components/PostEnd.tsx
+++ src/components/PostEnd.tsx
@@ -8,105 +8,105 @@ import { Icon } from "@iconify/react";
 import ImageWithFalldown from "components/ImageWithFalldown";
 
 export default async function PostEnd({ postInfo }: { postInfo: Post }) {
-    var res=await fetch(`${siteConfigs.backEndUrl}/get/post/postNavigation?slug=${postInfo.slug}`,{next:{tags:["posts"]}});
-    var previousPost:Post={},nextPost:Post={};
-    if(res.ok){
-        var postNavigation=await res.json();
-        previousPost=postNavigation.previous;
-        nextPost=postNavigation.next;
-    }
-    return <>
-        <div className="post-copyright">
-            <div className="post-copyright__title">
-                <span className="post-copyright-info">
-                    {postInfo.title}
-                </span>
-            </div>
-            <div className="post-copyright__type">
-                <span className="post-copyright-info">
-                    <a className="normal-a" href={`${siteConfigs.siteUrl}/posts/${postInfo.slug}/`}>
-                        {`${siteConfigs.siteUrl}/posts/${postInfo.slug}/`}
-                    </a>
-                </span>
-            </div>
-            <div className="post-copyright-m">
-            <div className="post-copyright-m-info">
-                <div className="post-copyright-a" style={{display:"inline-block",width:120}}>
-                    作者
-                <div className="post-copyright-cc-info">
-                    {siteConfigs.author}
-                </div>
-            </div>
-                <div className="post-copyright-c" style={{display:"inline-block",width:120}}>
-                    发布于
-                    <div className="post-copyright-cc-info">
-                        {moment.unix(postInfo.publishTime!).format('YYYY-MM-DD')}
-                    </div>
-                </div>
-                    <div className="post-copyright-u" style={{display:"inline-block",width:120}}>
-                        更新于
-                        <div className="post-copyright-cc-info">
-                            {moment.unix(postInfo.lastUpdatedTime!).format('YYYY-MM-DD')}
-                        </div>
-                    </div>
-                    <div className="post-copyright-c" style={{display:"inline-block",width:180}}>
-                        许可协议
-                        <div className="post-copyright-cc-info">
-                            <a className="icon" rel="noopener external nofollow noreferrer" target="_blank" title="Creative Commons" href="https://creativecommons.org/">
-                                <i className="fab fa-creative-commons"></i>
-                            </a>
-                            <a className="normal-a" rel="noopener external nofollow noreferrer" target="_blank" title="CC BY-NC-SA 4.0" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh">
-                                CC BY-NC-SA 4.0
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  var res=await fetch(`${siteConfigs.backEndUrl}/get/post/postNavigation?slug=${postInfo.slug}`,{next:{tags:["posts"]}});
+  var previousPost:Post={},nextPost:Post={};
+  if(res.ok){
+    var postNavigation=await res.json();
+    previousPost=postNavigation.previous;
+    nextPost=postNavigation.next;
+  }
+  return <>
+    <div className="post-copyright">
+      <div className="post-copyright__title">
+        <span className="post-copyright-info">
+          {postInfo.title}
+        </span>
+      </div>
+      <div className="post-copyright__type">
+        <span className="post-copyright-info">
+          <a className="normal-a" href={`${siteConfigs.siteUrl}/posts/${postInfo.slug}/`}>
+            {`${siteConfigs.siteUrl}/posts/${postInfo.slug}/`}
+          </a>
+        </span>
+      </div>
+      <div className="post-copyright-m">
+      <div className="post-copyright-m-info">
+        <div className="post-copyright-a" style={{display:"inline-block",width:120}}>
+          作者
+        <div className="post-copyright-cc-info">
+          {siteConfigs.author}
         </div>
-        <div id="postend-tagbar">
-            <div id="postend-tags">{
-                postInfo.tags!.map((tag, index) => {
-                    return <a className="postend-tag" key={index} href={`/tags/${tag}`} rel="noopener external nofollow noreferrer" target="_blank">{tag}</a>
-                })
-            }</div>
-            <ShareJs postInfo={postInfo}/>
+      </div>
+        <div className="post-copyright-c" style={{display:"inline-block",width:120}}>
+          发布于
+          <div className="post-copyright-cc-info">
+            {moment.unix(postInfo.publishTime!).format('YYYY-MM-DD')}
+          </div>
         </div>
-        <div id="postend-navigation">
-            {previousPost?
-                <Link className={`postend-navigation-item previous${nextPost?"":" single"}`} href={`/posts/${previousPost.slug}`}>
-                    <ImageWithFalldown className="postend-navigation-image" src={previousPost.bannerImg!} alt={previousPost.title!} falldownImg={siteConfigs.falldownImg}/>
-                    <span className="postend-navigation-headline">
-                        <span className="postend-navigation-intro">
-                            <Icon icon="fa6-solid:angle-left"/>上一篇
-                        </span>
-                        <span className="postend-navigation-date">
-                            <Icon icon="fa6-solid:calendar-days"/>
-                                {moment.unix(previousPost.publishTime!).format('YYYY-MM-DD')}
-                        </span>
-                    </span>
-                    <span className="postend-navigation-title">
-                        {previousPost.title}
-                    </span>
-                </Link>
-            :<></>}
-            {nextPost?
-                <Link className={`postend-navigation-item next${previousPost?"":" single"}`} href={`/posts/${nextPost.slug}`}>
-                    <ImageWithFalldown className="postend-navigation-image" src={nextPost.bannerImg?nextPost.bannerImg:siteConfigs.falldownImg} alt={nextPost.title!} falldownImg={siteConfigs.falldownImg}/>
-                    <span className="postend-navigation-headline">
-                        <span className="postend-navigation-date">
-                            <Icon icon="fa6-solid:calendar-days"/>
-                                {moment.unix(nextPost.publishTime!).format('YYYY-MM-DD')}
-                        </span>
-                        <span className="postend-navigation-intro">
-                            下一篇<Icon icon="fa6-solid:angle-right"/>
-                        </span>
-                    </span>
-                    <span className="postend-navigation-title">
-                        {nextPost.title}
-                    </span>
-                </Link>
-            :<></>}
+          <div className="post-copyright-u" style={{display:"inline-block",width:120}}>
+            更新于
+            <div className="post-copyright-cc-info">
+              {moment.unix(postInfo.lastUpdatedTime!).format('YYYY-MM-DD')}
+            </div>
+          </div>
+          <div className="post-copyright-c" style={{display:"inline-block",width:180}}>
+            许可协议
+            <div className="post-copyright-cc-info">
+              <a className="icon" rel="noopener external nofollow noreferrer" target="_blank" title="Creative Commons" href="https://creativecommons.org/">
+                <i className="fab fa-creative-commons"></i>
+              </a>
+              <a className="normal-a" rel="noopener external nofollow noreferrer" target="_blank" title="CC BY-NC-SA 4.0" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh">
+                CC BY-NC-SA 4.0
+              </a>
+            </div>
+          </div>
         </div>
-        <hr/>
-    </>
+      </div>
+    </div>
+    <div id="postend-tagbar">
+      <div id="postend-tags">{
+        postInfo.tags!.map((tag, index) => {
+          return <a className="postend-tag" key={index} href={`/tags/${tag}`} rel="noopener external nofollow noreferrer" target="_blank">{tag}</a>
+        })
+      }</div>
+      <ShareJs postInfo={postInfo}/>
+    </div>
+    <div id="postend-navigation">
+      {previousPost?
+        <Link className={`postend-navigation-item previous${nextPost?"":" single"}`} href={`/posts/${previousPost.slug}`}>
+          <ImageWithFalldown className="postend-navigation-image" src={previousPost.bannerImg!} alt={previousPost.title!} falldownImg={siteConfigs.falldownImg}/>
+          <span className="postend-navigation-headline">
+            <span className="postend-navigation-intro">
+              <Icon icon="fa6-solid:angle-left"/>上一篇
+            </span>
+            <span className="postend-navigation-date">
+              <Icon icon="fa6-solid:calendar-days"/>
+                {moment.unix(previousPost.publishTime!).format('YYYY-MM-DD')}
+            </span>
+          </span>
+          <span className="postend-navigation-title">
+            {previousPost.title}
+          </span>
+        </Link>
+      :<></>}
+      {nextPost?
+        <Link className={`postend-navigation-item next${previousPost?"":" single"}`} href={`/posts/${nextPost.slug}`}>
+          <ImageWithFalldown className="postend-navigation-image" src={nextPost.bannerImg?nextPost.bannerImg:siteConfigs.falldownImg} alt={nextPost.title!} falldownImg={siteConfigs.falldownImg}/>
+          <span className="postend-navigation-headline">
+            <span className="postend-navigation-date">
+              <Icon icon="fa6-solid:calendar-days"/>
+                {moment.unix(nextPost.publishTime!).format('YYYY-MM-DD')}
+            </span>
+            <span className="postend-navigation-intro">
+              下一篇<Icon icon="fa6-solid:angle-right"/>
+            </span>
+          </span>
+          <span className="postend-navigation-title">
+            {nextPost.title}
+          </span>
+        </Link>
+      :<></>}
+    </div>
+    <hr/>
+  </>
 }
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -8,63 +8,63 @@ import moment from "moment";
 import Link from "next/link";
 
 export default function SearchBox({show,closeFunction}:{closeFunction:Function,show:boolean}) {
-    const [results,setResults]=useState<ReactElement>();
-    const inputRef=useRef<HTMLInputElement>(null);
-    const processResults=async ()=>{
-        const query=inputRef.current?.value.trim();
-        if(query){
-            setResults(<div id="search-box-results">搜索中...</div>);
-            const res=await fetch(`${siteConfigs.backEndUrl}/get/post/searchPosts?query=${query}`,{next:{tags:["posts"]}});
-            if(res.ok){
-                const data:Post[]=(await res.json()).data;
-                setResults(<div id="search-box-results">共找到{data.length}篇文章{data.map(
-                    (post:Post)=>{
-                        return (
-                            <div className="search-box-result card-widget" key={post.slug}>
-                                <Link className="search-box-result-link" 
-                                    href={`/posts/${post.slug}`} 
-                                    onClick={()=>{closeFunction()}}
-                                    dangerouslySetInnerHTML={{
-                                        __html:post.title!.replace(eval(`/${query}/gi`),(q)=>`<span class="search-box-result-highlight">${q}</span>`)
-                                    }}
-                                />
-                                <div className="search-box-result-context" 
-                                    dangerouslySetInnerHTML={{__html:
-                                        post.context!.replace(eval(`/${query}/gi`),(q)=>`<span class="search-box-result-highlight">${q}</span>`)
-                                    }}/>
-                                <span className="search-box-result-time">
-                                    <Icon icon="fa6-solid:calendar"/>
-                                    {moment.unix(post.publishTime!).format("YYYY-MM-DD")}
-                                </span>
-                            </div>
-                        );
-                    }
-                )}</div>);
-            }
-            else{
-                setResults(<div id="search-box-results">搜索失败</div>);
-            }
-        }
+  const [results,setResults]=useState<ReactElement>();
+  const inputRef=useRef<HTMLInputElement>(null);
+  const processResults=async ()=>{
+    const query=inputRef.current?.value.trim();
+    if(query){
+      setResults(<div id="search-box-results">搜索中...</div>);
+      const res=await fetch(`${siteConfigs.backEndUrl}/get/post/searchPosts?query=${query}`,{next:{tags:["posts"]}});
+      if(res.ok){
+        const data:Post[]=(await res.json()).data;
+        setResults(<div id="search-box-results">共找到{data.length}篇文章{data.map(
+          (post:Post)=>{
+            return (
+              <div className="search-box-result card-widget" key={post.slug}>
+                <Link className="search-box-result-link" 
+                  href={`/posts/${post.slug}`} 
+                  onClick={()=>{closeFunction()}}
+                  dangerouslySetInnerHTML={{
+                    __html:post.title!.replace(eval(`/${query}/gi`),(q)=>`<span class="search-box-result-highlight">${q}</span>`)
+                  }}
+                />
+                <div className="search-box-result-context" 
+                  dangerouslySetInnerHTML={{__html:
+                    post.context!.replace(eval(`/${query}/gi`),(q)=>`<span class="search-box-result-highlight">${q}</span>`)
+                  }}/>
+                <span className="search-box-result-time">
+                  <Icon icon="fa6-solid:calendar"/>
+                  {moment.unix(post.publishTime!).format("YYYY-MM-DD")}
+                </span>
+              </div>
+            );
+          }
+        )}</div>);
+      }
+      else{
+        setResults(<div id="search-box-results">搜索失败</div>);
+      }
     }
-    const searchEnter=(e:React.KeyboardEvent<HTMLInputElement>)=>{
-        if(e.key=="Enter")
-            processResults();
-    }
-    return (
-        <>
-            <div id="search-box" className={`card-widget${show?" show":" hide"}`}>
-                <div id="search-box-header">
-                    <button id="search-box-searchbtn" onClick={processResults}>
-                        <Icon icon="fa6-solid:magnifying-glass" width="20" height="20" />
-                    </button>
-                    <input autoComplete="off" ref={inputRef} type="text" placeholder="搜索博文..." id="search-box-input" onKeyDown={searchEnter}/>
-                    <button id="search-box-close" onClick={()=>{closeFunction(!show)}}>
-                        <Icon icon="fa6-solid:xmark" width="20" height="20"/>
-                    </button>
-                </div>
-                {results}
-            </div>
-            <div id="search-box-mask" className={`${show?"show":"hide"}`} onClick={()=>{closeFunction(!show)}}/>
-        </>
-    );
+  }
+  const searchEnter=(e:React.KeyboardEvent<HTMLInputElement>)=>{
+    if(e.key=="Enter")
+      processResults();
+  }
+  return (
+    <>
+      <div id="search-box" className={`card-widget${show?" show":" hide"}`}>
+        <div id="search-box-header">
+          <button id="search-box-searchbtn" onClick={processResults}>
+            <Icon icon="fa6-solid:magnifying-glass" width="20" height="20" />
+          </button>
+          <input autoComplete="off" ref={inputRef} type="text" placeholder="搜索博文..." id="search-box-input" onKeyDown={searchEnter}/>
+          <button id="search-box-close" onClick={()=>{closeFunction(!show)}}>
+            <Icon icon="fa6-solid:xmark" width="20" height="20"/>
+          </button>
+        </div>
+        {results}
+      </div>
+      <div id="search-box-mask" className={`${show?"show":"hide"}`} onClick={()=>{closeFunction(!show)}}/>
+    </>
+  );
 }
