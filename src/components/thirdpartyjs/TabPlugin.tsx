68dc3875de7442df7b58b56893ada46fee974922
LYXOfficial <lyx2010official@outlook.com>
2024-09-27 18:43:20 +0800

    fix tab

--- src/components/thirdpartyjs/TabPlugin.tsx
+++ src/components/thirdpartyjs/TabPlugin.tsx
@@ -6,7 +6,7 @@ export default function TabPlugin() {
         document.querySelectorAll('.etag-tab-header').forEach(header=>{
             header.addEventListener('click',function(this:HTMLDivElement){
                 const index=this.dataset.index;
-                this.querySelectorAll('.etag-tab-header').forEach(h=>h.classList.remove('active'));
+                this.parentNode?.parentNode?.querySelectorAll('.etag-tab-header').forEach(h=>h.classList.remove('active'));
                 this.parentNode?.parentNode?.querySelectorAll('.etag-tab-body').forEach(body=>body.classList.remove('active'));
                 this.classList.add('active');
                 this.parentNode?.parentNode?.querySelector(`.etag-tab-body[data-index="${index}"]`)!.classList.add('active');
