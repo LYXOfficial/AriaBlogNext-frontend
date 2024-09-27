3f30e70f8e6428d1c1845f333ddcb228d2f712f3
LYXOfficial <lyx2010official@outlook.com>
2024-09-27 18:36:57 +0800

    fix tab bug

--- src/components/thirdpartyjs/TabPlugin.tsx
+++ src/components/thirdpartyjs/TabPlugin.tsx
@@ -6,10 +6,10 @@ export default function TabPlugin() {
         document.querySelectorAll('.etag-tab-header').forEach(header=>{
             header.addEventListener('click',function(this:HTMLDivElement){
                 const index=this.dataset.index;
-                document.querySelectorAll('.etag-tab-header').forEach(h=>h.classList.remove('active'));
-                document.querySelectorAll('.etag-tab-body').forEach(body=>body.classList.remove('active'));
+                this.querySelectorAll('.etag-tab-header').forEach(h=>h.classList.remove('active'));
+                this.querySelectorAll('.etag-tab-body').forEach(body=>body.classList.remove('active'));
                 this.classList.add('active');
-                document.querySelector(`.etag-tab-body[data-index="${index}"]`)!.classList.add('active');
+                this.querySelector(`.etag-tab-body[data-index="${index}"]`)!.classList.add('active');
             });
         });
     },[]);
