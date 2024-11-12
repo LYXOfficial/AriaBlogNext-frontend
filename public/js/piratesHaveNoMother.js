4d88f61c21cb4d0e08d8c19e0c52861249885300
LYXOfficial <lyx2010official@outlook.com>
2024-11-12 19:11:06 +0800

    add new domain

--- public/js/piratesHaveNoMother.js
+++ public/js/piratesHaveNoMother.js
@@ -1,5 +1,5 @@
 const daobansima="aHR0cHM6Ly9ibG9nLnlhcmlhLnRvcA==";
 const gobackplease=decodeURIComponent(escape(window.atob(daobansima)))
-if(!(document.location.href.startsWith('http://localhost')||document.location.href.startsWith(gobackplease))){
+if(!(document.location.href.startsWith('http://localhost')||document.location.href.startsWith(gobackplease)||document.location.href.startsWith("https://0v0.my"))){
     document.location.href=gobackplease+document.location.pathname;
 }
