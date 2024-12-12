ecb850a9843d00a772f8cad785f3c3de6b21499e
LYXOfficial <lyx2010official@outlook.com>
2024-12-13 01:11:51 +0800

    勿忘国耻，振兴中华

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import "styles/NavBar.css";
 import { Icon } from '@iconify/react';
 import Link from "next/link";
-import { MenuItems,siteConfigs } from "config";
+import { MenuItems, siteConfigs } from "config";
 import { useRouter } from "nextjs-toploader/app";
 import { throttle } from "lodash";
 import { usePathname } from 'next/navigation'
@@ -12,106 +12,106 @@ import { MenuItem, MenuItemChild } from "@/interfaces/menuitem";
 import React from "react";
 
 export default function NavBar() {
-    const router=useRouter();
-    const [trans,setTrans]=useState(false);
-    const [searchBoxShow,setSearchBoxShow]=useState(false);
-    const pathName=usePathname();
-    const scrollHandler=throttle(()=>{
-        if(document.location.href.includes("/posts/")){
-            if(document.documentElement.scrollTop<60){
+    const router = useRouter();
+    const [trans, setTrans] = useState(false);
+    const [searchBoxShow, setSearchBoxShow] = useState(false);
+    const pathName = usePathname();
+    const scrollHandler = throttle(() => {
+        if (document.location.href.includes("/posts/")) {
+            if (document.documentElement.scrollTop < 60) {
                 setTrans(true);
-            }else{
+            } else {
                 setTrans(false);
             }
         }
-        else{
+        else {
             setTrans(false);
         }
-    },100);
-    useEffect(()=>{
-        (window as any).toRandomPost=async ()=>{
-            const res=await fetch(`${siteConfigs.backEndUrl}/get/post/postSlugs`,{next:{revalidate:7200,tags:["posts"]}})
-            if(res.ok){
-                const posts:string[]=(await res.json()).data;
-                const randomIndex:number=Math.round(Math.random()*posts.length);
+    }, 100);
+    useEffect(() => {
+        (window as any).toRandomPost = async () => {
+            const res = await fetch(`${siteConfigs.backEndUrl}/get/post/postSlugs`, { next: { revalidate: 7200, tags: ["posts"] } })
+            if (res.ok) {
+                const posts: string[] = (await res.json()).data;
+                const randomIndex: number = Math.round(Math.random() * posts.length);
                 router.push(`/posts/${posts[randomIndex]}`);
             }
         };
         scrollHandler();
-        window.addEventListener("scroll",scrollHandler);
-        return ()=>{
-            window.removeEventListener("scroll",scrollHandler);
+        window.addEventListener("scroll", scrollHandler);
+        return () => {
+            window.removeEventListener("scroll", scrollHandler);
         }
-    },[]);
-    useEffect(scrollHandler,[pathName]);
-    const [hoveringElement,setHoveringElement]=useState("");
-    const [mobileMenuOpen,setMobileMenuOpen]=useState(false);
+    }, []);
+    useEffect(scrollHandler, [pathName]);
+    const [hoveringElement, setHoveringElement] = useState("");
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     return (<>
-        <nav id="navbar" className={trans?"trans":""}>
+        <nav id="navbar" className={trans ? "trans" : ""}>
             <Link id="site-name" href="/">{siteConfigs.title}</Link>
             <div id="menu-center">
-                <div id="menu-items" className={mobileMenuOpen?"mobile-menu-open":"mobile-menu-close"}>
-                    {MenuItems.map((item)=>{
+                <div id="menu-items" className={mobileMenuOpen ? "mobile-menu-open" : "mobile-menu-close"}>
+                    {MenuItems.map((item) => {
                         return (
                             <div className="menu-item" key={item.name}
-                                onMouseEnter={()=>setHoveringElement(item.name)} onMouseLeave={()=>setHoveringElement("")}>
-                                    {item.link?
-                                        <Link className="site-page" href={item.link}>
-                                            {item.icon}
-                                            <span>{" "+item.name}</span>
-                                        </Link>
-                                        :
-                                        <span className="site-page">
-                                            {item.icon}
-                                            <span>{" "+item.name}</span>
-                                        </span>
-                                    }
-                                    {item.childs.length?
-                                        <div className={"site-page-childs "+(hoveringElement==item.name?"show":"hide")}>
-                                            {item.childs.map((child:MenuItemChild)=>{
-                                                return (
-                                                    <Link href={child.link} key={child.name} className="site-page-child">
-                                                        {child.icon}
-                                                        <div>{child.name}</div>
-                                                    </Link>
-                                                );
-                                            })}
-                                        </div>
-                                    :""
-                                    }
+                                onMouseEnter={() => setHoveringElement(item.name)} onMouseLeave={() => setHoveringElement("")}>
+                                {item.link ?
+                                    <Link className="site-page" href={item.link}>
+                                        {item.icon}
+                                        <span>{" " + item.name}</span>
+                                    </Link>
+                                    :
+                                    <span className="site-page">
+                                        {item.icon}
+                                        <span>{" " + item.name}</span>
+                                    </span>
+                                }
+                                {item.childs.length ?
+                                    <div className={"site-page-childs " + (hoveringElement == item.name ? "show" : "hide")}>
+                                        {item.childs.map((child: MenuItemChild) => {
+                                            return (
+                                                <Link href={child.link} key={child.name} className="site-page-child">
+                                                    {child.icon}
+                                                    <div>{child.name}</div>
+                                                </Link>
+                                            );
+                                        })}
+                                    </div>
+                                    : ""
+                                }
                             </div>
                         )
                     })}
                 </div>
             </div>
             <div id="menu-buttons">
-                <button className="menu-button" title="搜索" onClick={()=>{
+                <button className="menu-button" title="搜索" onClick={() => {
                     setSearchBoxShow(!searchBoxShow);
-                    setTimeout(()=>{
+                    setTimeout(() => {
                         document.getElementById("search-box-input")?.focus();
-                    },10);
+                    }, 10);
                 }}>
-                    <Icon icon="fa6-solid:magnifying-glass"/>
+                    <Icon icon="fa6-solid:magnifying-glass" />
                 </button>
-                <button className="menu-button" title="随便逛逛" onClick={()=>{
+                <button className="menu-button" title="随便逛逛" onClick={() => {
                     (window as any).toRandomPost();
                 }}>
-                    <Icon icon="fa6-solid:paper-plane"/>
+                    <Icon icon="fa6-solid:paper-plane" />
                 </button>
-                <button className="menu-button" title="开往" onClick={()=>{
-                    window.location.href="https://travellings.cn/go.html";
+                <button className="menu-button" title="开往" onClick={() => {
+                    window.location.href = "https://travellings.cn/go.html";
                 }}>
-                    <Icon icon="fa6-solid:train-subway"/>
+                    <Icon icon="fa6-solid:train-subway" />
                 </button>
-                <button className="menu-button menu-button-mobmenu" title="展开菜单" onClick={()=>{
+                <button className="menu-button menu-button-mobmenu" title="展开菜单" onClick={() => {
                     setMobileMenuOpen(!mobileMenuOpen);
                 }}>
-                    <Icon icon="fa6-solid:bars"/>
+                    <Icon icon="fa6-solid:bars" />
                 </button>
             </div>
         </nav>
-        <div id="sidebar-mask" className={mobileMenuOpen?"active":"disactive"} onClick={()=>{setMobileMenuOpen(false)}}/>
-        <SearchBox show={searchBoxShow} closeFunction={setSearchBoxShow}/>
-        </>
+        <div id="sidebar-mask" className={mobileMenuOpen ? "active" : "disactive"} onClick={() => { setMobileMenuOpen(false) }} />
+        <SearchBox show={searchBoxShow} closeFunction={setSearchBoxShow} />
+    </>
     );
 }
--- src/components/RightButtons.tsx
+++ src/components/RightButtons.tsx
@@ -1,40 +1,40 @@
 "use client";
 import "styles/RightButtons.css";
 import { Icon } from "@iconify/react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-export function RightButtonsPosts({toggleBarrage}:{toggleBarrage:()=>void}){
-    const [rbHide,setRbHide]=useState(false);
-    const [tocHide,setTocHide]=useState(false);
-    useEffect(()=>{
-        const scrollHandler=()=>{
-            if(document.documentElement.scrollTop>100)
+export function RightButtonsPosts({ toggleBarrage }: { toggleBarrage: () => void }) {
+    const [rbHide, setRbHide] = useState(false);
+    const [tocHide, setTocHide] = useState(false);
+    useEffect(() => {
+        const scrollHandler = () => {
+            if (document.documentElement.scrollTop > 100)
                 setRbHide(false);
             else setRbHide(true);
         }
-        document.addEventListener("scroll",scrollHandler);
+        document.addEventListener("scroll", scrollHandler);
         scrollHandler();
-        return ()=>{
-            document.removeEventListener("scroll",scrollHandler);
+        return () => {
+            document.removeEventListener("scroll", scrollHandler);
         }
-    },[]);
-    function scrollToTop(){
-        document.documentElement.scroll({behavior:"smooth",top:0});
+    }, []);
+    function scrollToTop() {
+        document.documentElement.scroll({ behavior: "smooth", top: 0 });
     }
-    function scrollToComments(){
-        document.documentElement.scroll({behavior:"smooth",top:(document.querySelector("#post-comment-container") as HTMLElement).offsetTop-80});
+    function scrollToComments() {
+        document.documentElement.scroll({ behavior: "smooth", top: (document.querySelector("#post-comment-container") as HTMLElement).offsetTop - 80 });
     }
-    function setToc(){
+    function setToc() {
         setTocHide(!tocHide);
-        if(tocHide) document.querySelector(".card-toc")!.className="card-widget card-aside card-toc";
-        else document.querySelector(".card-toc")!.className="card-widget card-aside card-toc mobile-show";
+        if (tocHide) document.querySelector(".card-toc")!.className = "card-widget card-aside card-toc";
+        else document.querySelector(".card-toc")!.className = "card-widget card-aside card-toc mobile-show";
     }
-    return (<div id="rightbuttons" className={rbHide?"rb-hide":"rb-show"}>
+    return (<div id="rightbuttons" className={rbHide ? "rb-hide" : "rb-show"}>
         <button className="rightbutton rightbutton-toc" title="文章目录" onClick={setToc}>
             <Icon icon="fa6-solid:align-left" />
         </button>
         <button className="rightbutton rightbutton-barrageShow" title="开关评论浮窗" onClick={toggleBarrage}>
-            <Icon icon="mingcute:danmaku-fill"/>
+            <Icon icon="mingcute:danmaku-fill" />
         </button>
         <button className="rightbutton rightbutton-toTop" title="回到顶部" onClick={scrollToTop}>
             <Icon icon="fa6-solid:arrow-up" />
@@ -44,29 +44,29 @@ export function RightButtonsPosts({toggleBarrage}:{toggleBarrage:()=>void}){
         </button>
     </div>);
 }
-export function RightButtonsPages({toggleBarrage}:{toggleBarrage:()=>void}){
-    const [rbHide,setRbHide]=useState(false);
-    useEffect(()=>{
-        const scrollHandler=()=>{
-            if(document.documentElement.scrollTop>100)
+export function RightButtonsPages({ toggleBarrage }: { toggleBarrage: () => void }) {
+    const [rbHide, setRbHide] = useState(false);
+    useEffect(() => {
+        const scrollHandler = () => {
+            if (document.documentElement.scrollTop > 100)
                 setRbHide(false);
             else setRbHide(true);
         }
-        document.addEventListener("scroll",scrollHandler);
+        document.addEventListener("scroll", scrollHandler);
         scrollHandler();
-        return ()=>{
-            document.removeEventListener("scroll",scrollHandler);
+        return () => {
+            document.removeEventListener("scroll", scrollHandler);
         }
-    },[]);
-    function scrollToTop(){
-        document.documentElement.scroll({behavior:"smooth",top:0});
+    }, []);
+    function scrollToTop() {
+        document.documentElement.scroll({ behavior: "smooth", top: 0 });
     }
-    function scrollToComments(){
-        document.documentElement.scroll({behavior:"smooth",top:(document.querySelector("#post-comment-container") as HTMLElement).offsetTop-80});
+    function scrollToComments() {
+        document.documentElement.scroll({ behavior: "smooth", top: (document.querySelector("#post-comment-container") as HTMLElement).offsetTop - 80 });
     }
-    return (<div id="rightbuttons" className={rbHide?"rb-hide":"rb-show"}>
+    return (<div id="rightbuttons" className={rbHide ? "rb-hide" : "rb-show"}>
         <button className="rightbutton rightbutton-barrageShow" title="开关评论浮窗" onClick={toggleBarrage}>
-            <Icon icon="mingcute:danmaku-fill"/>
+            <Icon icon="mingcute:danmaku-fill" />
         </button>
         <button className="rightbutton rightbutton-toTop" title="回到顶部" onClick={scrollToTop}>
             <Icon icon="fa6-solid:arrow-up" />
@@ -76,23 +76,23 @@ export function RightButtonsPages({toggleBarrage}:{toggleBarrage:()=>void}){
         </button>
     </div>);
 }
-export function RightButtonsHome(){
-    const [rbHide,setRbHide]=useState(false);
-    useEffect(()=>{
-        const scrollHandler=()=>{
-            if(document.documentElement.scrollTop>100)
+export function RightButtonsHome() {
+    const [rbHide, setRbHide] = useState(false);
+    useEffect(() => {
+        const scrollHandler = () => {
+            if (document.documentElement.scrollTop > 100)
                 setRbHide(false);
             else setRbHide(true);
         }
-        document.addEventListener("scroll",scrollHandler);
-        return ()=>{
-            document.removeEventListener("scroll",scrollHandler);
+        document.addEventListener("scroll", scrollHandler);
+        return () => {
+            document.removeEventListener("scroll", scrollHandler);
         }
     });
-    function scrollToTop(){
-        document.documentElement.scroll({behavior:"smooth",top:0});
+    function scrollToTop() {
+        document.documentElement.scroll({ behavior: "smooth", top: 0 });
     }
-    return (<div id="rightbuttons" className={rbHide?"rb-hide":"rb-show"}>
+    return (<div id="rightbuttons" className={rbHide ? "rb-hide" : "rb-show"}>
         <button className="rightbutton rightbutton-toTop" title="回到顶部" onClick={scrollToTop}>
             <Icon icon="fa6-solid:arrow-up" />
         </button>
