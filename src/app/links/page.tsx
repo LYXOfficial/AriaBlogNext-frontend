33f7c02ac7e813270d86ebab205517b13ac8f20a
LYXOfficial <lyx2010official@outlook.com>
2024-12-18 18:01:20 +0800

    fix: building error

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,12 +1,9 @@
 import "styles/Pages.css";
 import "styles/PostContent.css";
-import FriendLinks from "components/FriendLinks";
 import { HomeRightSide } from "components/RightSide";
 import { siteConfigs } from "@/config";
 import React from "react";
-
-import HLJSNum from "components/thirdpartyjs/HLJSNum";
-import MDRenderer from "@/utils/mdrender";
+import Content from "./renderer";
 
 export const metadata = {
     title: "友链 | " + siteConfigs.title,
@@ -18,28 +15,7 @@ export default async function Page() {
             <div id="main-container" className="page">
                 <div id="article-container" className="page flink">
                     <div id="post-maincontent" className="page flink">
-                        <h1>友情链接</h1>
-                        <FriendLinks />
-                        <div dangerouslySetInnerHTML={{
-                            __html: await MDRenderer(
-                                `# 我的信息
-## Butterfly
-\`\`\`YAML
-- name: Ariasakaの小窝
-link: https://blog.yaria.top
-avatar: https://bu.dusays.com/2024/12/05/67517bcf104da.png
-descr: 人有悲欢离合 月有阴晴圆缺
-siteshot: https://bu.dusays.com/2024/09/19/66ec130ad1de0.png
-theme_color: '#ed709b'
-\`\`\`
-## HTML
-\`\`\`HTML
-<a href="https://blog.yaria.top"><img src="https://bu.dusays.com/2024/12/05/67517bcf104da.png" alt="avatar">Ariasakaの小窝</a>
-\`\`\`
-# 如何申请友链？
-咕咕咕。。。这样的话可以去留言板找我哦喵～`)
-                        }}></div>
-                        <HLJSNum />
+                        <Content />
                     </div>
                 </div>
                 <HomeRightSide />
--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -1,10 +1,5 @@
 import "styles/PostContent.css"
-
-import FancyBox from "components/thirdpartyjs/FancyBox";
-import HLJSNum from "components/thirdpartyjs/HLJSNum";
-import KaTex from "components/thirdpartyjs/KaTex";
 import { TwikooBaseComment, TwikooCountPost } from "components/thirdpartyjs/Twikoo";
-import Pangu from "components/thirdpartyjs/Pangu";
 import PostEnd from "components/PostEnd";
 import { Post } from "interfaces/post";
 import MDToTSXWithPlugins from "./mdxlibs";
@@ -13,16 +8,11 @@ export default function PostContent({ mdContent, postInfo }: { mdContent: string
   return (
     <article id="article-container" className="card-widget">
       <div id="post-maincontent">
-
         <MDToTSXWithPlugins mdContent={mdContent} />
       </div>
-      <FancyBox />
-      <HLJSNum />
-      <KaTex />
       <PostEnd postInfo={postInfo} />
       <TwikooBaseComment />
       <TwikooCountPost />
-      <Pangu container="article-container" />
     </article>
   );
 }
